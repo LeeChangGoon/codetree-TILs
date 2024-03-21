326a441c92e6fc05527740631a4bfc2224947cb0
이창환 <133215176+LeeChangGoon@users.noreply.github.com>
2024-03-21 11:11:26 +0900

    Title: 피보나치 수, Time: 187ms, Memory: 10MB, Status: Passed - CodeTree

--- 240321/피보나치 수/fibonacci-number.java
+++ 240321/피보나치 수/fibonacci-number.java
@@ -3,14 +3,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
-        int[] dp = new int[n+1];
-        dp[1]=1;
-        dp[2]=1;
+        int[] dp;
         if(n<=2) {
-            System.out.print(dp[n]);
+            System.out.print(1);
             return;
         }
         else {
+            dp=new int[n+1];
+        dp[1]=1;
+        dp[2]=1;           
             for (int i = 3; i <= n; i++) {
                 dp[i] = dp[i - 2] + dp[i - 1];
             }
