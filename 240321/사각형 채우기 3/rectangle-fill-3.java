e8a32476c55bc21b65c06dbe312a101f3a5d5d7c
이창환 <133215176+LeeChangGoon@users.noreply.github.com>
2024-03-21 14:06:48 +0900

    Title: 사각형 채우기 3, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240321/사각형 채우기 3/rectangle-fill-3.java
+++ 240321/사각형 채우기 3/rectangle-fill-3.java
@@ -7,10 +7,10 @@ public class Main {
         dp[1]=2;
         for(int i=2; i<=n; i++){
             for(int j=1; j<n; j++){
-                if(i/j>1){
+                if(i%j==0){
                     dp[i]+=dp[j]*(i/j);
                 }
-                else dp[i]+=dp[j]*(i/j + 1);
+                else dp[i]+=dp[j]*((i/j) + 1);
             }
             if(i%2==0) dp[i]+=3;
             else dp[i]+=2;
