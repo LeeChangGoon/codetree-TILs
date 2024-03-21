ad8a32084141ac7842a91e173f838e344f954438
이창환 <133215176+LeeChangGoon@users.noreply.github.com>
2024-03-21 11:18:27 +0900

    Title: 계단 오르기, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240321/계단 오르기/climbing-stairs.java
+++ 240321/계단 오르기/climbing-stairs.java
@@ -3,11 +3,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
+        if(n<=2){
+            System.out.print(1);
+            return;
+        }
         int[] dp = new int[n+1];
         dp[0]=0;
         dp[1]=0;
         dp[2]=1;
-        for(int i=3; i<=n; i++){
+        dp[3]=1;
+        for(int i=4; i<=n; i++){
             dp[i] = dp[i-2] %10007+ dp[i-3] % 10007;
         }
         System.out.print(dp[n]);
